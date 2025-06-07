e3ea846f83e1c4a2ade4df5434ef8d641f418d2b
ale99ms <martinez.ale9@gmail.com>
2025-06-07 12:30:46 +0200

    Modify create_project.tcl to include pwm_generator

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -13,9 +13,12 @@ set_property "target_language" "VHDL" [current_project]
 
 ## Add source files here
 add_files -fileset sources_1 ../rtl/counter.vhd
+add_files -fileset sources_1 ../rtl/pwm_generator.vhd
+add_files -fileset sources_1 ../rtl/my_pkg.vhd
 
 ## Add simulation files here
 add_files -fileset sim_1 ../tb/counter_tb.vhd
+add_files -fileset sim_1 ../tb/pwm_generator_tb.vhd
 
 ## Add constraint files here (only for FPGA implementation)
 #add_files -fileset constrs_1 ../constr/pynq.xdc
